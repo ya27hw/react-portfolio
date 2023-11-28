748135993de59b600abe173a773ef8ec87d24ec1
ya27hw <yousuf5550@gmail.com>
2023-11-28 12:24:18 +0400

    add stuff + formatting

--- app/page.tsx
+++ app/page.tsx
@@ -18,11 +18,11 @@ export default function Home() {
       </Head>
       {/* Main component is here */}
       <main className="px-5 md:px-20 lg:px-40 dark:bg-slate-800">
+        <div id="navbar-section">
+          <NavBar />
+        </div>
         {/* This section covers the NavBar, and the body below it */}
-        <section className="">
-          <div>
-            <NavBar />
-          </div>
+        <section className="min-h-screen">
           <div className="text-center p-10">
             <h2 className="text-5xl py-2 dark:text-teal-500 text-teal-600 font-medium md:text-6xl">
               Hey, I'm Yousuf!
@@ -30,9 +30,11 @@ export default function Home() {
             <h3 className="text-2xl pt-3 pb-2 md:text-3xl">
               Software Developer
             </h3>
-            <p className="text-md py-5 leading-8 md:text-2xl max-w-xl mx-auto">
-              Burtons is a new display font with cool characters. it's a
-              delightful font duo, serif and script perfect for headlines
+            <p className="text-lg py-5 leading-8 md:text-2xl max-w-xl mx-auto">
+              Very OP Software Developer. I am so good, I code and finish
+              projects in my sleep. I have developed Windows 17 ahead of
+              Microsoft. I live in the year 2050 with my technological
+              advancements.
             </p>
           </div>
           <div className="text-5xl flex justify-center gap-16 py-6  md:text-7xl">
@@ -45,8 +47,8 @@ export default function Home() {
           </div>
         </section>
         <section>
-          <div>
-            <h2 className="text-4xl lg:text-5xl pt-10 pb-3 dark:text-teal-500 text-teal-600 font-medium">
+          <div id="about-section">
+            <h2 className="text-4xl lg:text-5xl pb-3 dark:text-teal-500 text-teal-600 font-medium">
               About me
             </h2>
             <p className="text-md md:text-xl py-2 leading-8 px-10 max-w-6xl">
@@ -55,11 +57,16 @@ export default function Home() {
               watching my pidol Haaland why he didn't win Ballon d'or
             </p>
           </div>
-          <div className="lg:flex gap-10">
-            <Card cardTitle="Pionel Pandreas Pessi" />
-            <Card cardTitle="Pristiano Penaldo" />
-            <Card cardTitle="Pylian MPayPal" />
-            <Card cardTitle="Perling Paaland" />
+          <div id="projects-section">
+            <h2 className="text-4xl lg:text-5xl pt-20 pb-3 dark:text-teal-500 text-teal-600 font-medium">
+              Projects
+            </h2>
+            <div className="lg:flex gap-10" id="cards-section">
+              <Card cardTitle="Pionel Pandreas Pessi" />
+              <Card cardTitle="Pristiano Penaldo" />
+              <Card cardTitle="Pylian MPayPal" />
+              <Card cardTitle="Perling Paaland" />
+            </div>
           </div>
         </section>
         <section>
